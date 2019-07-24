bbdd111c7902a937a55e1dcb155a31b44da1233e
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-24 22:35:59 +0530

    src > components > search_bar.js : controlled component on input field added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,13 +5,13 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: '' };
+        this.state = { term: 'hello' };
     }
 
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({ term: event.target.value })} />
+                <input value={this.state.term} onChange={event => this.setState({ term: event.target.value })} />
                 Value of input : {this.state.term}
             </div>
         );
