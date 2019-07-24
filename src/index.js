43954c256b221c12e62daaf02a3416b6711575e9
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-24 23:16:41 +0530

    src > components > search_bar.js : YTSearch api import and console log result

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD7Z_tu0X_eHp4F5QWHr7V73vDCVIeNTyQ';
+
+YTSearch({key: API_KEY, term:'block chain'},function(data){
+    console.log(data);
+});
 
 // Create a new Component. That produce some HTML
 const App = () => {
